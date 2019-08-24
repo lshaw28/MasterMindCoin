006c9b31b0c9f197ee1b2c0a62beb24a5f1eb2df
lshaw28 <LAWCOLE2g-5g>
2019-08-24 15:24:58 -0500

    Key pair and public key addresses completed

--- wallet/index.js
+++ wallet/index.js
@@ -1,17 +1,17 @@
 const { STARTING_BALANCE } = require('../config');
 const { ec } = require('../util');
-const cryptohash = require('../util/crypto-hash');
+const cryptoHash = require('../util/crypto-hash');
 
 class Wallet {
     constructor() {
         this.balance = STARTING_BALANCE;
 
-        const keyPair = ec.genKeyPair();
+        this.keyPair = ec.genKeyPair();
 
         this.publicKey = this.keyPair.getPublic().encode('hex');
     }
 
-    sign() {
+    sign(data) {
         return this.keyPair.sign(cryptoHash(data))
     }
 };
